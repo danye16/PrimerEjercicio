fcc35437cec8d41e31799397964defd72dcd5080
Daniel Parra <danyel.parracarrillo@gmail.com>
2023-07-04 12:57:13 -0700

    Linea nueva

--- HolaMundo.sh
+++ HolaMundo.sh
@@ -1,5 +1,8 @@
+
 #!/bin/bash
 # Esta línea indica que el script debe ser interpretado por el intérprete de bash.
 
 # Imprime "Hola Mundo" en la consola.
 echo "Hola Mundo"
+echo "Mi nombre es Erik"
+
